19d0fa624c44bf628ab2235e8630ee706e7ca86d
innovateDTP <dhanyatc@gmail.com>
2017-05-21 03:18:47 -0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { CurrencyComponent }   from './currency/currency.component';
+import {ConvertRoutes} from './app.routes'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    RouterModule.forRoot(ConvertRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
